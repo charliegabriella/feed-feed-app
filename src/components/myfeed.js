fee034224cfc4eabbb5253774daad140d5fc47cc
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-12 16:55:07 +0100

    the wisdom if matias

--- src/components/myfeed.js
+++ src/components/myfeed.js
@@ -4,31 +4,56 @@ export default class MyFeed extends React.Component {
   // the initial state
   state = {
     loading: true,
-    data: "string"
+    data: []
   };
   // ...
-  componentDidMount() {
-    return fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then(res => res.json())
-      .then(data => {
-        console.log("did i fetch data?");
-        console.log(data.meals[0].strMeal);
-        this.setState({
-          loading: false,
-          data: data.meals[0].strMeal
-        });
-      });
+  componentDidMount = async () => {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const parsedResponse = await response.json();
+
+    const response2 = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const parsedResponse2 = await response2.json();
+
+    const response3 = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const parsedResponse3 = await response3.json();
+
+    const response4 = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const parsedResponse4 = await response4.json();
+
+    const response5 = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const parsedResponse5 = await response5.json();
+
+    const randomMeals = [
+      parsedResponse.meals[0],
+      parsedResponse2.meals[0],
+      parsedResponse3.meals[0],
+      parsedResponse4.meals[0],
+      parsedResponse5.meals[0]
+    ];
+
+    this.setState({ loading: false, data: randomMeals });
     // (1) fetch some data (hopefully, something resembling a list of articles)
     // (2) put it in component local state (as per the shape discussed above)
     // (3) ...and if the loading fails, set an error state like discussed above
-  }
+  };
 
   render() {
-    console.log(this.state.data.meals);
-
+    //console.log(this.state.data.meals);
+    const mealNames = this.state.data.map(meal => meal.strMeal);
+    console.log(mealNames);
     return (
       <div>
-        <ul>{this.state.data}</ul>
+        <ul>{mealNames.join(" - ")}</ul>
       </div>
     );
   }
